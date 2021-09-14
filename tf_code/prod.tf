f9b97076bfd1b0e9a5accfe3b48fe4f4f3a2f326
Kish Sal <kishan.salgado@gmail.com>
2021-09-13 23:04:33 -0400

    Added aws_eip and aws_instance

--- tf_code/prod.tf
+++ tf_code/prod.tf
@@ -57,3 +57,24 @@ resource "aws_security_group" "prod_web" {
         Terraform = "true"
     }
 }
+
+resource "aws_instance" "prod_web" {
+    ami             = "ami-0ed34781dc2ec3964"
+    instance_type   = "t2.nano"
+
+    vpc_security_group_ids = [
+        aws_security_group.prod_web.id
+    ]
+
+    tags = {
+        Terraform = "true"
+    }
+}
+
+resource "aws_eip" "prod_web" {
+    instance = aws_instance.prod_web.id
+
+    tags = {
+        Terraform = "true"
+    }
+}

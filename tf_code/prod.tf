c9fd6122be316733c5a81dbd967099b12b7b2082
Kish Sal <kishan.salgado@gmail.com>
2021-09-13 22:36:30 -0400

    Updated prod.tf with ingress and egress details

--- tf_code/prod.tf
+++ tf_code/prod.tf
@@ -8,6 +8,52 @@ resource "aws_s3_bucket" "prod_tf_course" {
     acl = "private"
 }
 
-resource "aws_default_vpc" "defautl" {
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "prod_web" {
+    name        = "prod_web"
+    description = "Allows http and https for inbound and outbound"
     
+    ingress = [{
+            description = "HTTP"
+            from_port   = 80
+            to_port     = 80
+            protocol    = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
+            security_groups = []
+            self = true
+        },
+        {
+            description = "HTTPS"
+            from_port   = 443
+            to_port     = 443
+            protocol    = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
+            security_groups = []
+            self = true
+         }
+    ]
+
+    egress = [{
+        description = "OPEN"
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = []
+        prefix_list_ids = []
+        security_groups = []
+        self = true
+        }
+    ]
+
+    tags = {
+        Terraform = "true"
+    }
 }
